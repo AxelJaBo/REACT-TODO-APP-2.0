d6aec15e8f5c28b474552fd7e739ac9b680832f8
AxelJaBo <axel.910921@gmail.com>
2023-11-20 20:48:02 -0800

    React.Children y React.cloneElement

--- src/TodoCounter/index.jsx
+++ src/TodoCounter/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './TodoCounter.css';
 
-function TodoCounter({completedTodos, totalTodos}) {
+function TodoCounter({completedTodos, totalTodos, loading}) {
   return (
-    <h1 className="TodoCounter">
+    <h2 
+      className={`TodoCounter ${!!loading && "TodoCounter--loading"}`}>
       Has completado <span>{completedTodos}</span> de <span>{totalTodos}</span> TODOs
-    </h1>
+    </h2>
   );
 }
 
