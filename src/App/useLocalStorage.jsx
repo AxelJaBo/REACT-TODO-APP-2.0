f9b36caee7f996ae563aaa959ab0ca807bfadbb8
AxelJaBo <axel.910921@gmail.com>
2023-11-21 21:09:03 -0800

    Completando el StorageEventListener

--- src/App/useLocalStorage.jsx
+++ src/App/useLocalStorage.jsx
@@ -4,6 +4,7 @@ function useLocalStorage(itemName, initialValue) {
     const [item, setItem] = React.useState(initialValue);
     const [loading, setLoading] = React.useState(true);
     const [error, setError] = React.useState(false);
+    const [sincronizedItem, setSincronizedItem] = React.useState(true);
 
     React.useEffect(()=> {    
         setTimeout(()=> {
@@ -18,19 +19,25 @@ function useLocalStorage(itemName, initialValue) {
                     setItem(parsedItem);
                 }
                 setLoading(false);
+                setSincronizedItem(true);
             }catch(error){
                 setLoading(false);
                 setError(true);
             }
         }, 2000);
-    }, []);
+    }, [sincronizedItem]);
 
     const saveItem = (newItem) => {
         localStorage.setItem('TODOS_V1', JSON.stringify(newItem));
         setItem(newItem);
     };
 
-    return {item, saveItem, error, loading};
+    const sincronizeItem = () => {
+        setLoading(true);
+        setSincronizedItem(false);
+    }
+
+    return {item, saveItem, error, loading, sincronizeItem};
 }
 
 export { useLocalStorage };
--- src/App/useTodos.jsx
+++ src/App/useTodos.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useLocalStorage } from "./useLocalStorage";
 
 function useTodos() {
-    const { item: todos, saveItem: saveTodos, loading, error } = useLocalStorage('TODOS_V1', []);
+    const { item: todos, saveItem: saveTodos, sincronizeItem: sincronizeTodos, loading, error } = useLocalStorage('TODOS_V1', []);
     const [searchValue, setSearchValue] = React.useState('');
     const [openModal, setOpenModal] = React.useState(false);
 
@@ -60,6 +60,7 @@ function useTodos() {
             deleteTodo,
             openModal,
             setOpenModal,
+            sincronizeTodos
         }
     );
 };
